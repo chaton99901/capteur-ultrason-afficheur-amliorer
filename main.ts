41210aac6a5f481097f9251492aebdc7c407993e
chaton99901 <94067162+chaton99901@users.noreply.github.com>
2021-11-22 09:47:45 -0500

    Mettre à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,92 @@
+let distance = 0
 basic.forever(function () {
-	
+    distance = sonar.ping(
+    DigitalPin.P0,
+    DigitalPin.P1,
+    PingUnit.Centimeters
+    )
+    if (distance > 25) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (distance > 20) {
+        basic.showLeds(`
+            # . # . #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (distance > 15) {
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (distance > 10) {
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (distance > 5) {
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            `)
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            `)
+    }
 })
